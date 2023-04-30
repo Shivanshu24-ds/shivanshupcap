72dced196b775eba9f30bf3fe75bf1bcc2e001de
Shivanshu24-ds <shivanshupandey.ds@gmail.com>
2023-04-30 08:43:25 +0530

    commit

--- test_app.py
+++ test_app.py
@@ -5,4 +5,4 @@ def test_app():
     response = app.test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b"<h3>Hello World, my name is Aamir Ansari</h3>"
+    assert response.data == b"<h3>Hello World, my name is Shivanshu Pandey</h3>"
